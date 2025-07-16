f02cd65d2cd735e3e30bc77cd72c5d283c6ae3e6
code-x-yash <yashrajwanshii@gmail.com>
2025-07-16 22:15:48 +0530

    test commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import Image from "next/image"
+import Image from "next/image" 
 import { Button } from "@/components/ui/button"
 import { ChevronLeft, ChevronRight, BookOpen, Coffee, Heart, Users } from "lucide-react"
 
